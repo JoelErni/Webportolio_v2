1d9169a446a4b838b9c1796507fa284b9b14263a
Joel Erni - Desktop <joel.erni@hispeed.ch>
2023-10-18 21:42:31 +0200

    intro + about me

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-var c1 = '#080908';
-var c2 = '#E6E8e6';
-var c3 = '#3772FF';
-var c4 = '#DF2935';
-var c5 = '#FDCA40';
-
 window.addEventListener("scroll", scroll);
-
-setInterval(() => {
-    background();
-}, 100);
-
-var background_rotation = 0;
-function background(){
-    document.querySelector('html').style = `background-image: linear-gradient(${background_rotation}deg, ${c3}, ${c4}, ${c5});`
-    background_rotation++;
-}
 
 function scroll() {
     intro_animation()
@@ -23,6 +7,32 @@ function scroll() {
 function intro_animation(){
     document.getElementById("intro").style.filter = `blur(${window.scrollY/25}px)`
 }
+
+addEventListener("mousemove", (event) => {});
+
+onmousemove = (event) => {
+    var mousePos = { x: event.clientX, y: event.clientY };
+    const screen_width = screen.availWidth;
+    const screen_height = screen.availHeight;
+    
+    var rotation_sensitivity = 25;
+    var shadow_offset = 50;
+
+    var x = (mousePos.y - (screen_height / 2)) / screen_height;
+    var y = (mousePos.x - (screen_width / 2)) / screen_width;
+
+    document.getElementById("title_container").style.transform = `rotateX(${x * rotation_sensitivity}deg) rotateY(${-y * rotation_sensitivity}deg)`
+
+    document.getElementById("title_name").style.textShadow = `${-y * shadow_offset}px ${-x * shadow_offset}px #080908`
+    document.getElementById("title_underline").style.boxShadow = `${-y * shadow_offset / 2.5}px ${-x * shadow_offset  / 2.5}px #080908`
+    document.getElementById("title_desc").style.textShadow = `${-y * shadow_offset / 5}px ${-x * shadow_offset / 5}px #080908`
+
+    document.getElementById("intro_arrow").style.filter = `drop-shadow(${-y * shadow_offset / 10}px ${-x * shadow_offset / 10}px #080908)`
+
+
+    console.log(x_rotation, y_rotation)
+};
+
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
