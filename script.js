a574f0cd4f6e8cc1f4032cd151fd2f10635a65c4
Joel Erni - Desktop <joel.erni@hispeed.ch>
2023-10-19 20:53:02 +0200

    asd

--- script.js
+++ script.js
@@ -10,26 +10,30 @@ function intro_animation(){
 
 addEventListener("mousemove", (event) => {});
 
-const shadow_color = '#080908'
+const shadow_color = ['#080908', '#E6E8E6']
+
 const screen_width = screen.availWidth;
 const screen_height = screen.availHeight;
 const rotation_sensitivity = 50;
 const shadow_offset = 50;
+
+//id, shadow_offset, color, sens, center origin
 const title_elements = [
-    ['title_name', 1],
-    ['title_underline', 0.4],
-    ['title_desc', 0.2],
-    ['intro_arrow', 0.1]
+    ['title_name', 1, 0, 1],
+    ['title_underline', 0.4, 0, 1],
+    ['title_desc', 0.2, 0, 1],
+    ['intro_arrow', 0.1, 0, 1],
 ]
+
 onmousemove = (event) => {
+    var mousePos = { x: event.clientX, y: event.clientY };
     for(i = 0; i < title_elements.length; i++){
         var element = document.getElementById(title_elements[i][0]);
-        var mousePos = { x: event.clientX, y: event.clientY };
         var x = (mousePos.y - screen_height / 2) / screen_height;
         var y = (mousePos.x - screen_width / 2) / screen_width;
 
         element.style.transform = `rotateX(${x * rotation_sensitivity}deg) rotateY(${-y * rotation_sensitivity}deg)`;
-        element.style.filter = `drop-shadow(${-y * shadow_offset * title_elements[i][1]}px ${-x * shadow_offset * title_elements[i][1]}px ${shadow_color})`
+        element.style.filter = `drop-shadow(${-y * shadow_offset * title_elements[i][1]}px ${-x * shadow_offset * title_elements[i][1]}px ${shadow_color[title_elements[i][2]]})`
     }
 };
 
